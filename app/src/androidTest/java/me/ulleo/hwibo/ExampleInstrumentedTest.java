a122789e812957793f972128e75b99fd6f885d3c
ulleo <ulleo.wang@gmail.com>
2017-01-24 15:53:36 +0800

    change flavors

--- app/src/androidTest/java/me/ulleo/hwibo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/me/ulleo/hwibo/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("me.ulleo.hwbo", appContext.getPackageName());
+        assertEquals("me.ulleo.hwibo", appContext.getPackageName());
     }
 }
